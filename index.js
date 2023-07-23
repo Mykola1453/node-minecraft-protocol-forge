0f7662b5f8b9406637e0bc178efdd9fa55cad074
Mykola1453 <69845760+Mykola1453@users.noreply.github.com>
2023-07-23 18:03:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 
 module.exports = {
   forgeHandshake: require('./src/client/forgeHandshake'),
+  forgeHandshake2: require('./src/client/forgeHandshake2'),
+  forgeHandshake3: require('./src/client/forgeHandshake3'),
   autoVersionForge: require('./src/client/autoVersionForge')
 };
